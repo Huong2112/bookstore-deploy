ef662e3955f5d31763997393b98590a873ebabc6
Damlinh0102 <dtlinh010202@gmail.com>
2023-04-02 16:15:17 +0700

    get item shopping cart

---
+++ src/main/java/hanu/edu/application/shoppingCart/controller/GetItemShoppingCartController.java
@@ -0,0 +1,20 @@
+package hanu.edu.application.shoppingCart.controller;
+
+import hanu.edu.domain.shoppingCart.service.GetItemsShoppingCartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GetItemShoppingCartController {
+    @Autowired
+    private GetItemsShoppingCartService getItemsShoppingCartService;
+
+    @GetMapping("/cart")
+    public ResponseEntity<?> getItem(@RequestParam long customerId) {
+        return new ResponseEntity<>(getItemsShoppingCartService.getItem(customerId), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/hanu/edu/domain/shoppingCart/service/GetItemsShoppingCartService.java
@@ -0,0 +1,54 @@
+package hanu.edu.domain.shoppingCart.service;
+
+import hanu.edu.domain.item.model.Item;
+import hanu.edu.domain.product.model.Product;
+import hanu.edu.domain.product.repository.ProductRepository;
+import hanu.edu.domain.shoppingCart.model.ShoppingCart;
+import hanu.edu.domain.shoppingCart.repository.ShoppingCartRepository;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GetItemsShoppingCartService {
+    @Autowired
+    private ShoppingCartRepository shoppingCartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<OutputCart> getItem(long customerId) {
+        ShoppingCart cart = shoppingCartRepository.getByCustomerId(customerId);
+        List<OutputCart> outputCarts = new ArrayList<>();
+        for(Item i: cart.getItems()) {
+            Product product = productRepository.getById(i.getProductId());
+            OutputCart outputCart = OutputCart.builder()
+                    .productId(product.getId())
+                    .description(product.getDescription())
+                    .name(product.getName())
+                    .price(product.getPrice())
+                    .inStock(product.getInStock())
+                    .images(product.getImages())
+                    .rate(product.getRate())
+                    .quantity(i.getQuantity()).build();
+            outputCarts.add(outputCart);
+        }
+        return outputCarts;
+    }
+}
+@Builder
+@Getter
+class OutputCart {
+    private long quantity;
+    private long productId;
+    private String name;
+    private double price;
+    private String description;
+    private long inStock;
+    private List<String> images;
+    private float rate;
+}
