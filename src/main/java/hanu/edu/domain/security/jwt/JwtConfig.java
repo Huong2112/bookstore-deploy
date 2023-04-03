6f3efc77e329cdb59cd96366588b3ee944bad7d5
Damlinh0102 <dtlinh010202@gmail.com>
2023-04-03 18:31:59 +0700

    change token expiration time & cart quantity

--- src/main/java/hanu/edu/domain/security/jwt/JwtConfig.java
+++ src/main/java/hanu/edu/domain/security/jwt/JwtConfig.java
@@ -14,7 +14,7 @@ public class JwtConfig {
     @Value("${jwt.prefix:Bearer}")
     private String prefix;
 
-    @Value("${jwt.expiration:#{60*60}}")
+    @Value("${jwt.expiration:#{60*60*24}}")
     private int expiration;
 
     @Value("${jwt.secret:7A24432646294A404E635266556A586E327234753778214125442A472D4B6150}")
