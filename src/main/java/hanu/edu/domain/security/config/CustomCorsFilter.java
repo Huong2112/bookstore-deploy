eac1563cfe948baff99c340e296bee66230e6c73
hoangvythptlhp0710 <hoangvytb7891@gmail.com>
2023-05-08 19:01:31 +0700

    config cors

--- src/main/java/hanu/edu/domain/security/config/CustomCorsFilter.java
+++ src/main/java/hanu/edu/domain/security/config/CustomCorsFilter.java
@@ -20,7 +20,7 @@ public class CustomCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With, X-Auth-Token");
 //        response.setHeader("Access-Control-Allow-Credentials", "true");
